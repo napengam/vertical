6f8a4024626480bd16bb318dc30599fe08e7ae29
Heinz <Heinz@Heinz-PC.fritz.box>
2020-06-07 11:00:46 +0200

    new simpler code

--- js/vertical.js
+++ js/vertical.js
@@ -1,6 +1,30 @@
+
 function rotateHeadCell(tableId) {
     'use strict';
-    var aRows = document.getElementById(tableId).rows, padding = 4;
+    var t, p;
+    t = document.getElementById(tableId);
+    p = t.querySelectorAll('[data-rotate]');
+    p.forEach((th) => {
+        var div, w, h;
+        div = document.createElement('DIV');
+        div.innerHTML = th.innerHTML;
+        div.style.display = 'inline-block';
+        th.innerHTML = '';
+        th.appendChild(div);
+        div.style.transformOrigin = 'top left';
+        div.style.transform = 'rotate(-90deg)';
+        div.style.whiteSpace = 'nowrap';
+        w = div.clientHeight;
+        h = div.clientWidth;
+        div.style.width = w + 'px';
+        div.style.height = h + 'px';
+        div.style.position = 'relative';
+        div.style.top = div.clientHeight + 'px';
+    });
+}
+function rotateHeadCellOld(tableId) {
+    'use strict';
+    var aRows = document.getElementById(tableId).rows, padding = 0;
     [].every.call(aRows, function (row) {
         if (row.cells[0].tagName !== 'TH') {
             return false;
@@ -12,20 +36,17 @@ function rotateHeadCell(tableId) {
         var rotate = [], maxw = -1;
         [].forEach.call(row.cells, function (cell) {
             var w;
-            if (!cell.hasAttribute("data-rotate")) {
-                cell.vAlign = 'bottom';
-                return;
+            if (cell.hasAttribute("data-rotate")) {
+                rotate.push(cell);
+                cell.vAlign = 'middle';
+                cell.innerHTML = '<div style="transform: rotate(-90deg);white-space: nowrap; float:left; width:auto;">' + cell.innerHTML + '</div>';
+                w = cell.clientWidth;
+                if (w > maxw) {
+                    maxw = w;
+                    cell.style.height = maxw + padding + 'px';
+                }
+                cell.firstChild.style.width = cell.firstChild.clientHeight + 'px';
             }
-            rotate.push(cell);
-            cell.vAlign = 'middle';
-            cell.innerHTML = '<div style="transform: rotate(-90deg);white-space: nowrap; float:left; width:auto;">' + cell.innerHTML + '</div>';
-            w = cell.clientWidth;
-            if (w > maxw) {
-                maxw = w;
-                cell.style.height = maxw + padding + 'px';
-            }
-            cell.firstChild.style.width = cell.firstChild.clientHeight + 'px';
-            
         });
         if (maxw === -1) {
             return;
